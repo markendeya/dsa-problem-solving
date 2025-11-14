d8fd3b1166f414ff3cb214178369498d5b29cde9
markendeya <markendeya.nakka@gmail.com>
2025-11-14 14:59:40 -0600

    first commit

---
+++ src/SimplifyPath.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class Simplypath {
+
+    public String simplifyPath(String path) {
+        if(path== null || path.isEmpty()){
+            return "";
+        }
+        Stack<String> stack = new Stack<>();
+        for(String str: path.split("/")){
+            if(str.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }else if(!str.isEmpty() && !str.equals(".")){
+                    stack.push(str);
+                }
+        }
+        return "/" + String.join("/",stack);
+    }
+
+    public static void main(String[] args) {
+        Simplypath sol = new Simplypath();
+        // Test cases
+        System.out.println(sol.simplifyPath("/a//b////c/d//././/..")); // Expected output: "/a/b/c"
+        System.out.println(sol.simplifyPath("/../")); // Expected output: "/"
+        System.out.println(sol.simplifyPath("/home//foo/")); // Expected output: "/home/foo"
+    }
+}
