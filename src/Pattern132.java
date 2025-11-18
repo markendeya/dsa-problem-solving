a5cb28a3940b7bc45e5086c92ea29fc16124d21a
markendeya <markendeya.nakka@gmail.com>
2025-11-18 16:40:00 -0600

    132 pattern

---
+++ src/Pattern132.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Pattern132 {
+    public boolean find132pattern(int[] nums) {
+        int third = Integer.MIN_VALUE;
+        Stack<Integer> st = new Stack<Integer>();
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<third) return true;
+            while(!st.isEmpty() && st.peek()< nums[i]){
+                third = st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+}
