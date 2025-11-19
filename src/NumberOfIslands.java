8b0e14cd5d41bac9e352e59de590c99591f04598
markendeya <markendeya.nakka@gmail.com>
2025-11-19 11:57:40 -0600

    BFS Number of Islands

--- src/NumberOfIslands.java
+++ src/NumberOfIslands.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NumberOfIslands {
     public int countIslands(int[][] grid) {
         int row = grid.length;
@@ -22,6 +25,25 @@ public class NumberOfIslands {
         dfs(row+1,col,grid,m,n);
         dfs(row,col-1,grid,m,n);
         dfs(row,col+1,grid,m,n);
+    }
+    private static void visitIslandBFS(int[][] matrix, int x, int y) {
+        Queue<int[]> neighbors = new LinkedList<>();
+        neighbors.add(new int[] { x, y });
+        while (!neighbors.isEmpty()) {
+            int row = neighbors.peek()[0];
+            int col = neighbors.peek()[1];
+            neighbors.remove();
+
+            if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] == 0)
+                continue;
+            matrix[row][col] = 0;
+
+            // insert all neighboring cells to the queue for BFS
+            neighbors.add(new int[] { row + 1, col }); // lower cell
+            neighbors.add(new int[] { row - 1, col }); // upper cell
+            neighbors.add(new int[] { row, col + 1 }); // right cell
+            neighbors.add(new int[] { row, col - 1 }); // left cell
+        }
     }
 
     public static void main(String[] args) {
