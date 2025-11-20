1d2ef3e6c01f362f2973382512f7f9cf5741a924
markendeya <markendeya.nakka@gmail.com>
2025-11-20 15:16:37 -0600

    find smallest missing positive number

---
+++ src/FindSmallestMissingPositiveNumber.java
@@ -0,0 +1,24 @@
+public class FindSmallestMissingPositiveNumber {
+    public int findNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1])
+                swap(nums, i, nums[i] - 1);
+            else
+                i++;
+        }
+
+        for (i = 0; i < nums.length; i++)
+            if (nums[i] != i + 1)
+                return i + 1;
+
+        return nums.length + 1;
+    }
+
+    // swap swaps the elements at indices i and j in the array.
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
